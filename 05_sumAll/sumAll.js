9c5d51ca1eec4df846a81aa61d8373a51c955c1e
Jaret Balba <Jaretzbalba@gmail.com>
2022-10-05 16:10:16 -0700

    completed exercises 1-6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+  if (a < 0 || b < 0) return 'ERROR';
+  return Array.from({ length: Math.abs(a - b) + 1 }, (_, i) => i + 1).reduce((a, b) => a + b, 0);
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // leap year if divisible by 4
+  // leap year if NOT dividisble by 100
+  // leap year if divisible by 400
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? true : false;
 };
 
 // Do not edit below this line
